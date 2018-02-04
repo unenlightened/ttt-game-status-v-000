b22037bfcf6889b15a3778d7c090aa196ac6d8e7
Chely Ho <adept_inept@yahoo.com>
2018-02-04 15:18:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,9 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[4]
 
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
 
   end
 
