b3e5cc5824d48789b6986308c43f0f3d5b3b20b9
Chely Ho <adept_inept@yahoo.com>
2018-02-04 15:19:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,11 @@ def won?(board)
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
+    if position_1 =="X" && position_2 == "X" && position_3 == "X"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
   end
 
 
