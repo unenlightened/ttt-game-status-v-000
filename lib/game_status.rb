322c9727dbf0bdf1b92a14fb32a67a0de688e191
Chely Ho <adept_inept@yahoo.com>
2018-02-04 15:17:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,10 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[4]
 
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
   end
 
 
